17001a20a75db4e58fbb69b19fee8a1772215bb4
NbekWeb <nbekdev@gmail.com>
2024-10-26 14:02:21 +0500

    first commit

--- src/components/FirstForm.vue
+++ src/components/FirstForm.vue
@@ -10,7 +10,7 @@
         :class="
           gender == 1
             ? 'text-blue-700 border-blue-700 border'
-            : 'text-blue-200 border-transparent'
+            : 'text-blue-200 border-blue-200 '
         "
         @click="changeGender(1)"
       >
